946e7cb5f1656eb32dc6648dac37994a93e83142
JonasHalver <44400498+JonasHalver@users.noreply.github.com>
2018-11-01 13:25:35 +0100

    Water physics

---
+++ Assets/WaterPhysics.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPhysics : MonoBehaviour {
+
+    public GameObject water;
+    public Collider2D ballCollider;
+    public BoxCollider2D waterCollider;
+    public Rigidbody2D ballRB;
+    public float waterDrag;
+
+
+	
+	void Start () {
+        waterCollider = water.GetComponent<BoxCollider2D>();
+        ballRB = this.GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update () {
+       if (ballCollider.IsTouching(waterCollider) == true)
+            {
+            ballRB.gravityScale = -1f;
+            ballRB.drag = waterDrag;
+            }
+        else
+            {
+            ballRB.gravityScale = 1f;
+            ballRB.drag = 0f;
+            }
+        }
+}
